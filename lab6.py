54161f2a0d4e7a0053bc9631f6f5498621602f7e
esmyhecha <ehechavarria@ufl.edu>
2023-03-22 14:38:26 -0400

    this is lab 6

---
+++ lab6.py
@@ -0,0 +1,36 @@
+# ESMERALDA HECHAVARRIA
+
+def encode_input(input_e):
+    encoded_res = ''
+    for char in input_e:
+        char = int(char)
+        char += 3
+        encoded_res += str(char)
+    return encoded_res
+
+
+def main():
+1    while True:
+        print()
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        option = input("Please enter an option: ")
+
+        if option == '1':
+            input_e = input("Please enter your password to encode: ")
+            encode_input(input_e)
+            print("Your password has been encoded and stored!")
+
+        if option == '2':
+            pass
+
+        if option == '3':
+            break
+
+if __name__ == "__main__":
+    main()
